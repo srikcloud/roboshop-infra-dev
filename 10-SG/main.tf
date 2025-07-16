63a19843914a98c13fc5c0360dcce8a57a54bdb2
srikanth <srikanth.baireddy@outlook.com>
2025-07-16 16:38:41 +0530

    terraform

---
+++ 10-SG/main.tf
@@ -0,0 +1,9 @@
+module "frontend" {
+  # source = "../../terraform-aws-securitygroup"
+  source = "git::https://github.com/srikcloud/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = var.frontend_sg_name
+  sg_description = var.frontend_sg_description
+  vpc_id = local.vpc_id
+}

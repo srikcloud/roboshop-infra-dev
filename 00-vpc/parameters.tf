55648cbf3a1c6b63a593bc69a03e5bbc4db36149
srikanth <srikanth.baireddy@outlook.com>
2025-07-18 00:31:40 +0530

    terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_ids)
+}

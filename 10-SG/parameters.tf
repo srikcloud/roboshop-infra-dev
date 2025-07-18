f69e235938f88a1ecd3a1b3c874be9d1671262eb
srikanth <srikanth.baireddy@outlook.com>
2025-07-19 00:44:40 +0530

    terraform

--- 10-SG/parameters.tf
+++ 10-SG/parameters.tf
@@ -13,5 +13,5 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
 resource "aws_ssm_parameter" "backend_alb_sg_id" {
   name  = "/${var.project}/${var.environment}/backend_alb_sg_id"
   type  = "String"
-  value = backend_alb.sg_id
+  value = module.backend_alb.sg_id
 }

995b9bc717ee60dbc64d0421cd1f96d19c385c79
srikanth <srikanth.baireddy@outlook.com>
2025-07-18 19:32:57 +0530

    terraform

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -21,3 +21,7 @@ data "aws_ami" "joindevops" {
 data "aws_ssm_parameter" "bastion_sg_id" {
 name  = "/${var.project}/${var.environment}/bastion_sg_id"
 }
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+name  = "/${var.project}/${var.environment}/public_subnet_ids"
+}

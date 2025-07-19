24d463b2cf60efb1a3ac37a43a964fedde82dab4
srikanth <srikanth.baireddy@outlook.com>
2025-07-19 16:53:16 +0530

    terraform

--- 10-SG/main.tf
+++ 10-SG/main.tf
@@ -28,6 +28,16 @@ module "backend_alb" {
   vpc_id = local.vpc_id
 }
 
+module "vpn" {
+  # source = "../../terraform-aws-securitygroup"
+  source = "git::https://github.com/srikcloud/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+  sg_name = "vpn"
+  sg_description = "for vpn"
+  vpc_id = local.vpc_id
+}
+
 # bastion accepting connections from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -47,3 +57,40 @@ resource "aws_security_group_rule" "backed_alb_bastion" {
   source_security_group_id = module.bastion.sg_id
   security_group_id = module.backend_alb.sg_id
 }
+
+#vpn ports 22, 443, 1194, 943
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+} 

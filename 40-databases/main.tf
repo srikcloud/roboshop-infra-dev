975809732cb996527bc153195430bf5919fab7d1
srikanth <srikanth.baireddy@outlook.com>
2025-07-20 10:55:42 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -80,6 +80,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_id
+# iam_instance_profile = "EC2RoleToFetchSSMParams"
 
   tags = merge (
     local.common_tags,

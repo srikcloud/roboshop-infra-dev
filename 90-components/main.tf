742429280d378b07ce4fd8c1288b720a2d5c5fad
srikanth <srikanth.baireddy@outlook.com>
2025-08-14 15:55:38 +0530

    terraform commit

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "../../terraform-aws-roboshop"
+    source = "git::https://github.com/srikcloud/terraform-aws-roboshop.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
